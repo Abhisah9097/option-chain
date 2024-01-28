e3a205272190b18f494839960d6405e7ae96adc2
Abhishek Kumar <aksing9097@gmail.com>
2024-01-29 01:03:43 +0530

    set build file

---
+++ build/precache-manifest.cf227ea0c242ea8400f2653b5945b63f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ff8e53376e813bcc0b966be451ba5bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75d1b487c7eebb24a32b",
+    "url": "/static/css/2.f9e8a227.chunk.css"
+  },
+  {
+    "revision": "e367813cb1d9618dacd2",
+    "url": "/static/css/main.2a987b3d.chunk.css"
+  },
+  {
+    "revision": "75d1b487c7eebb24a32b",
+    "url": "/static/js/2.06d3cce3.chunk.js"
+  },
+  {
+    "revision": "e367813cb1d9618dacd2",
+    "url": "/static/js/main.45c25327.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
